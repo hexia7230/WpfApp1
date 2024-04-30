2bde210ce803b83c9c291b32e546c59aaea1bedd
Sho <Sho@HEX01>
2024-04-30 16:56:47 +0800

    プロジェクト ファイルを追加します。

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DebugButton.Click += DebugButton_Click;
+        }
+
+        private void DebugButton_Click(object sender, RoutedEventArgs e)
+        {
+            // PasswordBoxを非表示にする
+            PasswordBox.Visibility = Visibility.Hidden;
+
+            // FucListとSubWindowを表示する
+            FucList.Visibility = Visibility.Visible;
+            SubWindow.Visibility = Visibility.Visible;
+        }
+    }
+}
